e9f99a6b0fba54b49b42293190abfd74d8d75aa6
denizzeybek <zeybekdeniz64@gmail.com>
2023-11-28 23:05:15 +0300

    fix

--- app/[[...category]]/page.js
+++ app/[[...category]]/page.js
@@ -2,25 +2,20 @@ import HomeContainer from '@/containers/home';
 import Movies from '@/mocks/movies';
 import Genres from '@/mocks/genres';
 
-const delay = async (ms) => {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  };
-async function Home({ params }) {
-  await delay(1000);
-
+function Home({ params }) {
   let categoryId = params.category;
   const selectedCategoryMovies = Movies.results.filter((movie) =>
-    movie.genre_ids.includes(+categoryId)
+    movie.genre_ids.includes(+categoryId),
   );
   const selectedCategory = Genres.genres.find(
-    (genre) => genre.id === +categoryId
+    (genre) => genre.id === +categoryId,
   );
   return (
     <HomeContainer
       selectedCategory={{
         id: categoryId,
         movies: selectedCategoryMovies || [],
-        title: selectedCategory?.name
+        title: selectedCategory?.name,
       }}
     />
   );
--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // styledComponents: true
-  output: 'export',
 };
 
 module.exports = nextConfig;
