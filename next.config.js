b6538a96eed3f3a804f04b87edde36856f8d9749
denizzeybek <zeybekdeniz64@gmail.com>
2023-11-28 22:54:55 +0300

    fix deploy problem

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    styledComponents: true
-}
+  // styledComponents: true
+  trailingSlash: true,
+  output: 'export',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
